ac4e9cf4d9026f0e7805d4fc75247adb16301d28
Topi Ilmoni <topi.ilmoni@gmail.com>
2023-11-29 20:00:34 +0200

    Added first tests

---
+++ tests/components/Button.test.js
@@ -0,0 +1,21 @@
+import Button from '../../src/components/Button';
+import {render, fireEvent, screen} from '@testing-library/react';
+
+it('Should render the component', async () => {
+  render(<Button className={'btn-click'}>Click me!</Button>);
+  expect(screen.getByText('Click me!'));
+});
+
+it('Should call onClick function', async () => {
+  const mockCallback = jest.fn(() => {});
+
+  render(
+    <Button className={'btn-click'} onClick={mockCallback}>
+      Click me!
+    </Button>
+  );
+
+  fireEvent.click(screen.getByText('Click me!'));
+
+  expect(mockCallback.mock.calls).toHaveLength(1);
+});
