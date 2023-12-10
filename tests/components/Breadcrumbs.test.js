cc21f3db69e5a9df57eef0aaec0c019fccd62cfe
Topi Ilmoni <topi.ilmoni@gmail.com>
2023-12-10 17:20:45 +0200

    Added tests to Breadcrumbs

--- tests/components/Breadcrumbs.test.js
+++ tests/components/Breadcrumbs.test.js
@@ -1,16 +1,45 @@
 import Breadcrumbs from '../../src/components/Breadcrumbs';
-import {render} from '@testing-library/react';
-it('Should render component', () => {
-  const RouteItems = [
-    {
-      title: 'Page One',
-    },
-    {
-      title: 'Page Two',
-    },
-    {
-      title: 'Page Three',
-    },
-  ];
-  render(<Breadcrumbs breadcrumbs={RouteItems} />);
+import {render, screen} from '@testing-library/react';
+import '@testing-library/jest-dom';
+
+describe('Breadcrumbs', () => {
+  it('Should render component', () => {
+    const RouteItems = [
+      {
+        title: 'Page One',
+        path: 'path1',
+      },
+      {
+        title: 'Page Two',
+        path: 'path2',
+      },
+      {
+        title: 'Page Three',
+        path: 'path2',
+      },
+    ];
+    render(<Breadcrumbs breadcrumbs={RouteItems} />);
+
+    expect(screen.getByText('Page One'));
+    expect(screen.getByText('Page Two'));
+    expect(screen.getByText('Page Three'));
+  });
+
+  it('Should skip breadcrumb if specified', () => {
+    const RouteItems = [
+      {
+        title: 'Page One',
+        path: 'path1',
+        skipBreadcrumb: true,
+      },
+      {
+        title: 'Page Two',
+        path: 'path2',
+      },
+    ];
+    render(<Breadcrumbs breadcrumbs={RouteItems} />);
+
+    expect(() => screen.getByText('Page One')).toThrow();
+    expect(screen.getByText('Page Two'));
+  });
 });
